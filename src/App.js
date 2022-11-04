b7a4a98a2fd2e390525989b03a2576074634c425
tetianaShev2 <105647410+tetianaShev2@users.noreply.github.com>
2022-11-04 20:24:37 +0100

    imported search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import "./App.css";
-import Weather from "./Weather";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello React</h1>
-        <Weather city="Paris" />
-      </header>
-    </div>
+export default function App() {
+  let [city, setCity] = useState("null");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState(null);
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function submittedCity(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1b92415f0f2a81ca6f05973c2626e33c&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  let form = (
+    <form onSubmit={submittedCity}>
+      <input type="search" onChange={updateCity} />
+      <input type="submit" value="search" />
+    </form>
   );
-}
 
-export default App;
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
